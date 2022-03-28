f6c26e1c6cd22e8dd4530e8279b85344ed03b4b6
Isaiah <ijandicos90@gmail.com>
2022-03-28 18:48:06 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+  function superbowlWin(record) {
+const win = record.find(function(game){
+    return game.result === "W"
+})
+if (win){
+    return win.year
+}
+else{
+    return undefined
+}
+} 
+  
+
+
+
